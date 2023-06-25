7450f4324ddbb11f9088b9abbd16ba4bab1ec0bb
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-06-25 09:02:45 +0200

    Dodanie towrzenia plików log

--- skrypt.sh
+++ skrypt.sh
@@ -5,3 +5,24 @@ then
     current_date=$(date +"%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
 fi
+
+if [ "$1" == "--logs" ]
+then
+    # Ustalamy nazwę skryptu
+    script_name=$(basename "$0")
+    
+    # Iterujemy od 1 do 100
+    for i in {1..100}
+    do
+        # Tworzymy nazwę pliku
+        file_name="log$i.txt"
+        
+        # Pobieramy aktualną datę
+        current_date=$(date)
+        
+        # Wpisujemy informacje do pliku
+        echo "Nazwa pliku: $file_name" > $file_name
+        echo "Nazwa skryptu: $script_name" >> $file_name
+        echo "Data: $current_date" >> $file_name
+    done
+fi
