a19771645e3c1dd3d5a9edb00621f093a9d5fb92
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-06-25 10:17:40 +0200

    Update log

--- skrypt.sh
+++ skrypt.sh
@@ -30,8 +30,19 @@ if [ "$1" == "--logs"  ] && [[ $2 =~ ^[0-9]+$ ]] || [ "$1" == "-l"  ] && [[ $2 =
 then
     for ((i=1; i<=$2; i++))
     do
-        touch "log$i.txt"
-        echo "Utworzono plik log$i.txt"
+        # Pobieramy aktualną datę
+        current_date=$(date)
+        
+        # Tworzenie katalogu
+        mkdir -p "log$i"
+        
+        # Tworzenie pliku
+        touch "log$i/log$i.txt"
+        
+        # Dodawanie informacji do pliku
+        echo "Nazwa pliku: log$i/log$i.txt" >> "log$i/log$i.txt"
+        echo "Nazwa skryptu: $script_name" >> "log$i/log$i.txt"
+        echo "Data: $current_date" >> "log$i/log$i.txt"
     done
 elif [ "$1" == "--logs" ] || [ "$1" == "-l" ]
 then
@@ -41,15 +52,19 @@ then
     #Iterujemy od 1 do 100
     for i in {1..100}
     do
-        # Tworzymy nazwę pliku
-        file_name="log$i.txt"
         
         # Pobieramy aktualną datę
         current_date=$(date)
         
-        # Wpisujemy informacje do pliku
-        echo "Nazwa pliku: $file_name" > $file_name
-        echo "Nazwa skryptu: $script_name" >> $file_name
-        echo "Data: $current_date" >> $file_name
+        # Tworzenie katalogu
+        mkdir -p "log$i"
+        
+        # Tworzenie pliku
+        touch "log$i/log$i.txt"
+        
+        # Dodawanie informacji do pliku
+        echo "Nazwa pliku: log$i/log$i.txt" >> "log$i/log$i.txt"
+        echo "Nazwa skryptu: $script_name" >> "log$i/log$i.txt"
+        echo "Data: $current_date" >> "log$i/log$i.txt"
     done
 fi
